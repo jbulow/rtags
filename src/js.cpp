851dabaa105a020f2d2287f2fa6fb61227f35de0
Anders Bakken <agbakken@gmail.com>
2013-03-17 22:55:21 -0700

    More work on js. Now using esprima!!

--- src/js.cpp
+++ src/js.cpp
@@ -23,12 +23,138 @@ v8::Handle<v8::String> toJson(v8::Handle<v8::Value> obj)
 
 struct Cursor
 {
-    String target;
+    Cursor()
+        : length(0), parent(-1), type(Invalid) {}
+    int length, parent;
     Set<int> references;
+    String name;
+    enum Type {
+        Invalid,
+        Declaration,
+        Reference
+    } type;
 };
+
+inline Log operator<<(Log log, const Cursor &cursor)
+{
+    log << String::format<128>("name: %s length: %d type: %s",
+                               cursor.name.constData(),
+                               cursor.length,
+                               cursor.type == Cursor::Declaration ? "declaration" :
+                               (cursor.type == Cursor::Reference ? "reference" : "invalid"));
+    return log;
+}
+
+class Visitor
+{
+public:
+    bool visit(v8::Handle<v8::Object> object);
+
+    const Map<int, Cursor> &cursors() const { return mCursors; }
+private:
+    bool visitRecursive(v8::Handle<v8::Object> object);
+    bool visitDeclarations(v8::Handle<v8::Array> object);
+
+    Map<int, Cursor> mCursors;
+};
+
+template <typename T>
+static v8::Handle<T> get(v8::Handle<v8::Object> object, const char *property)
+{
+    v8::HandleScope scope;
+    v8::Handle<v8::Value> prop = object->Get(v8::String::New(property));
+    return scope.Close(v8::Handle<T>::Cast(prop));
+}
+
+template <typename T>
+static v8::Handle<T> get(v8::Handle<v8::Array> object, int index)
+{
+    v8::HandleScope scope;
+    v8::Handle<v8::Value> prop = object->Get(index);
+    return scope.Close(v8::Handle<T>::Cast(prop));
+}
+
+static inline bool operator==(const char *l, v8::Handle<v8::String> r)
+{
+    return r.IsEmpty() ? -1 : !strcmp(l, *v8::String::Utf8Value(r));
+}
+
+static inline bool operator==(v8::Handle<v8::String> l, const char *r)
+{
+    return l.IsEmpty() ? 1 : !strcmp(*v8::String::Utf8Value(l), r);
+}
+
+#define toCString(str) *v8::String::Utf8Value(str)
+
+bool Visitor::visit(v8::Handle<v8::Object> object)
+{
+    v8::HandleScope handleScope;
+    assert(!object.IsEmpty());
+    // v8::Handle<v8::Value> b = parse->Get(v8::String::New("body"));
+    // printf("isnull %d\n", b.IsEmpty());
+    // printf("foo[%s]\n", toCString(toJson(b)));
+
+    v8::Handle<v8::Array> body = get<v8::Array>(object, "body");
+    if (body.IsEmpty()) {
+        return false;
+    }
+
+    for (unsigned i=0; i<body->Length(); ++i) {
+        if (!visitRecursive(get<v8::Object>(body, i))) {
+            error("Invalid body element at index %d", i);
+        }
+    }
+    return true;
+}
+
+bool Visitor::visitRecursive(v8::Handle<v8::Object> object)
+{
+    v8::HandleScope handleScope;
+    if (object.IsEmpty())
+        return false;
+
+    v8::Handle<v8::String> type = get<v8::String>(object, "type");
+    assert(!type.IsEmpty());
+    if (type == "VariableDeclaration") {
+        visitDeclarations(get<v8::Array>(object, "declarations"));
+    } else {
+        printf("%s\n", toCString(type));
+        // printf("[%s] %s:%d: } else { [after]\n", __func__, __FILE__, __LINE__);
+    }
+    return true;
+}
+
+bool Visitor::visitDeclarations(v8::Handle<v8::Array> declarations)
+{
+    if (declarations.IsEmpty() || !declarations->IsArray()) {
+        return false;
+    }
+    for (unsigned i=0; i<declarations->Length(); ++i) {
+        v8::Handle<v8::Object> declaration = get<v8::Object>(declarations, i);
+        assert(!declaration.IsEmpty());
+        v8::Handle<v8::String> type = get<v8::String>(declaration, "type");
+        if (type == "VariableDeclarator") {
+            v8::Handle<v8::Object> id = get<v8::Object>(declaration, "id");
+            assert(!id.IsEmpty() && id->IsObject());
+            v8::Handle<v8::String> name = get<v8::String>(id, "name");
+            v8::Handle<v8::Array> range = get<v8::Array>(id, "range");
+            assert(!range.IsEmpty() && range->Length() == 2);
+            const int offset = get<v8::Integer>(range, 0)->Value();
+            const int length = get<v8::Integer>(range, 1)->Value() - offset;
+            Cursor c;
+            c.name = String(toCString(name), name->Length());
+            c.type = Cursor::Declaration;
+            c.length = length;
+            mCursors[offset] = c;
+        }
+    }
+    return true;
+}
+
 
 int main(int argc, char **argv)
 {
+    Map<int, Cursor> cursors;
     if (argc < 2) {
         printf("[%s] %s:%d: if (argc < 2) {} [after]\n", __func__, __FILE__, __LINE__);
         return 1;
@@ -62,7 +188,7 @@ int main(int argc, char **argv)
     }
     v8::Handle<v8::Value> result = script->Run();
     v8::String::Utf8Value var(result);
-    printf("Result [%s]\n", *var);
+    // printf("Result [%s]\n", *var);
 
     v8::Handle<v8::Object> global = ctx->Global();
     assert(!global.IsEmpty());
@@ -86,6 +212,11 @@ int main(int argc, char **argv)
     args[1] = options;
     result = parse->Call(esprima, 2, args);
 
-    printf("Parse: %s\n", *v8::String::Utf8Value(toJson(result)));
+    if (!getenv("NO_JSON"))
+        printf("Parse: %s\n", *v8::String::Utf8Value(toJson(result)));
+
+    Visitor visitor;
+    visitor.visit(result->ToObject());
+    error() << visitor.cursors();
     return 0;
 }
